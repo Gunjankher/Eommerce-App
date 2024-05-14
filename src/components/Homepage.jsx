a3ca185e18414ce280f3a81f5fcc064d013beec8
Gunjankher <gkhercode@gmail.com>
2024-05-14 14:50:57 +0530

    ........

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,7 +6,7 @@ import Navbar from './Navbar'
 function Homepage() {
   return (
     <div className='max-w-[1600px] min-w-[1000px] bg-slate-300'>
-      <Navbar />
+      {/* <Navbar /> */}
 <Carousel />
 <h1 className='m-4'>The product card will be here</h1>
 <CarouselProduct />
