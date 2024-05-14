63b0c12cdd8a4def2b581c97c14c40a7209ff7f4
Gunjankher <gkhercode@gmail.com>
2024-05-14 14:50:19 +0530

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Route,RouterProvider,createBrowserRouter,createRoutesFromElements } from 'react-router-dom'
+import { Route,RouterProvider,createBrowserRouter,createRoutesFromElements, Routes } from 'react-router-dom'
 import Homepage from './components/Homepage.jsx'
-import {Checkout,ProductPage,SearchResults} from './components/index.js'
+import SearchResults from './components/SearchResults.jsx'
+import ProductPage from './components/ProductPage.jsx'
+import Checkout from './components/Checkout.jsx'
+import Layout from './components/Layout.jsx'
+
 
 
 const router = createBrowserRouter(
 createRoutesFromElements(
-  <Route path='/' element={<Homepage/>}>
-  <Route path='/search' element={<SearchResults/>} />
-  <Route path='/product' element={<ProductPage />} />
-  <Route path='/checkout' element={<Checkout />} />
- </Route>
-  
+  <Route path='' element = {<Layout />}>
+  <Route path="/" element={<Homepage />} />
+  <Route path="/search" element={<SearchResults />} />
+  <Route path="/product/:id" element={<ProductPage />} />
+  <Route path="/checkout" element={<Checkout />} />
+</Route>
 )
 
 )
+
+
+
 
 
 
