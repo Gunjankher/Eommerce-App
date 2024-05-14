5aeb73f88498f1ea1d28f4c577afe020b85b3937
Gunjankher <gkhercode@gmail.com>
2024-05-14 14:51:32 +0530

    routing complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import {ShoppingCartIcon} from '@heroicons/react/24/outline'
 import Search from './Search'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
    <header className='max-w-[1700px]'>
+    
 <div className='bg-amazonclone text-white flex h-[60px]' >
   {/* left   */}
 <div className='flex items-center'>
 
 <div className='flex pl-4'>
+    <Link to={'/'}>
     <img className = 'w-[100px] h-[35px]' src="../images/amazon.png" alt="" />
+    </Link>
 </div>
 
 <div className=' pr-4 pl-4'>
@@ -43,9 +46,9 @@ function Navbar() {
 </div>
 
 <div  className='flex items-center pr-5'>
-    <Link to={'/checkout'}>
+    <NavLink to={"/checkout"}>
 <ShoppingCartIcon  className='h-[48px] flex'/>
-    </Link>
+    </NavLink>
 <div className='relative'> 
 <div className='absolute right-[9px] font-bold m-2 -mt-4 text-orange-400'>
 0
